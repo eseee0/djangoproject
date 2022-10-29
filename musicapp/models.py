8090d6b7accdb0c840cd8905560431cec5437d2f
eseee0 <ada.oyugbo@gmail.com>
2022-10-29 10:32:29 +0100

    second push

--- musicapp/models.py
+++ musicapp/models.py
@@ -1,3 +1,30 @@
+from email.policy import default
+from unittest.util import _MAX_LENGTH
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+
+class Artiste(models.Model):
+    first_name= models.CharField(max_length=400)
+    last_name= models.CharField(max_length=400)
+    age= models.IntegerField()
+    def __str__(self):
+        return self.name
+
+class Song(models.Model):
+    Artiste = models.ForeignKey(Artiste, on_delete=models.CASCADE)
+    title= models.CharField(max_length=400)
+    date_released= models.DateField(default=datetime.today)
+    likes= models.CharField(max_length=400)
+    artiste_id= models.IntegerField()
+    def __str__(self):
+        return self.name
+
+class Lyric(models.Model):
+    Song = models.ForeignKey(Song, on_delete=models.CASCADE)
+    content= models.CharField(max_length=400)
+    song_id= models.IntegerField()
+
+    def __str__(self):
+        return self.name
